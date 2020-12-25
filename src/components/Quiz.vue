4f0adb8cd6137acbbb9388500e6b682f37326e41
AndrewBeznosko <andrew.beznosko@gmail.com>
2020-12-25 13:33:02 +0200

    style upd

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="vue-quiz">
     <div class="card bg-secondary">
-      <div class="card-header">
-        <h2 class="fw-bold m-0">Question question question?</h2>
+      <div class="card-header position-relative border-0">
+        <h2 class="fw-bold mb-1">Question question question?</h2>
+        <div class="progress position-absolute w-100 start-0 bottom-0 rounded-0" style="height: 2px;">
+          <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
+        </div>
       </div>
       <div class="card-body py-5">
         <div class="card">
           <!-- is-invalid -->
-          <!-- was-validated -->
-          <form class="list-group list-group-flush text-dark">
+          <!-- was-validated -->  
+          <form class="was-validated list-group list-group-flush text-dark">
             <div class="form-check list-group-item ps-5">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
               <label class="form-check-label fw-bold" for="flexRadioDefault1">
                 Default radio
               </label>
             </div>
             <div class="form-check list-group-item  ps-5">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" required>
               <label class="form-check-label fw-bold" for="flexRadioDefault2">
                 Default checked radio
               </label>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="home bg-dark vh-100 text-white">
-    <div class="container d-flex flex-column align-items-center justify-content-center h-100 pb-5">
-      <h1 class="text-center mb-5">Vue-quiz</h1>
-      <Quiz />
+  <div class="home bg-dark text-white">
+    <div class="container d-flex flex-column align-items-center justify-content-center min-vh-100 pb-5">
+      <h1 class="text-center mb-5 mt-3">Vue-quiz</h1>
+      <div class="mb-lg-5">
+          <Quiz />
+      </div>
     </div>
   </div>
 </template>
