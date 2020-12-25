da8bc502cc8799ddcd7c9f2cdcfbee4e01fc6a88
AndrewBeznosko <andrew.beznosko@gmail.com>
2020-12-25 17:55:39 +0200

    nav among questions

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,48 +1,56 @@
 <template>
   <div class="vue-quiz">
     <form 
+      @submit.prevent
+      v-if="currentQuestion"
       class="card bg-secondary"
       :class="{ 'was-validated' : was_validated }"
     >
       <div class="card-header position-relative border-0">
-        <h2 class="fw-bold mb-1">Question question question?</h2>
+        <h2 class="fw-bold mb-1">{{ currentQuestion.title }}</h2>
         <div class="progress position-absolute w-100 start-0 bottom-0 rounded-0" style="height: 2px;">
           <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
         </div>
       </div>
-      <div class="card-body py-5">
+      <div class="card-body py-3" style="min-height: 250px;">
         <div class="card">
           <div class="list-group list-group-flush text-dark">
 
-            <div class="form-check list-group-item ps-5">
+            <label
+              v-for="(answer, i) in currentQuestion.answers"
+              :key="`answer-${current_index}-${i}`"
+              class="form-check list-group-item ps-5"
+            >
               <input 
+                @change="questions[current_index].answer = answer.value"
+                :value="answer.value"
+                :name="`answerRadio-${currentQuestion.key}`" 
+                :id="`answer-${current_index}-${i}`" 
+                v-bind="{ 'checked' : currentQuestion.answer == answer.value }"
                 class="form-check-input" 
                 type="radio" 
-                name="flexRadioDefault" 
-                id="flexRadioDefault1" 
                 required
               >
-              <label 
-                class="form-check-label fw-bold" 
-                for="flexRadioDefault1"
-              >
-                Default radio
-              </label>
-            </div>
-
-            <div class="form-check list-group-item  ps-5">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" required>
-              <label class="form-check-label fw-bold" for="flexRadioDefault2">
-                Default checked radio
-              </label>
-            </div>
+              <span class="form-check-label fw-bold">{{ answer.title }}</span>
+            </label>
 
           </div>
         </div>
       </div>
       <div class="card-footer d-flex justify-content-between">
-        <button type="button" class="btn btn-outline-light disabled">Preview</button>
-        <button @click.prevent="validate($event)" type="button" class="btn btn-primary ml-auto fw-bold">Step</button>
+        <button 
+          @click="toPreventStep()" 
+          type="button" 
+          class="btn btn-outline-light"
+          v-bind="{ 'disabled' : current_index <= 0 }"
+        >
+          Prevent
+        </button>
+        <button 
+          @click.prevent="!isLastQuestion ? validate($event) : showResults()" 
+          type="button" 
+          class="btn btn-primary ml-auto fw-bold"
+        >{{ isLastQuestion ? 'Finish' : 'Step' }}</button>
       </div>
     </form>
   </div>
@@ -52,23 +60,58 @@
   export default {
     name: 'Quiz',
     props: {
-      msg: String
+      questionsProp: {
+        type: Array, 
+        default: [], 
+      }
     },
     data() {
       return {
         was_validated: false,
+        questions: [],
         current_index: 0
+      }
+    },
+    computed: {
+      totalQuestions() {
+        return this.questions.length
+      },  
+      isLastQuestion() {
+        return (this.questions.length-1 == this.current_index) ? true : false
+      },
+      currentQuestion() {
+        return this.filteredQuestions[this.current_index]
+      },
+      filteredQuestions: {
+        get() {
+          return this.questions.map((el) => {
+            if(!el['answer']) el['answer'] = ''
+
+            return el
+          })
+        },
+        set(questions) {
+          this.questions = questions
+        }
       }
     },
     methods: {
       validate(e) {
         e.target.closest('form').requestSubmit()
         this.was_validated = true
-        this.toNextStep()
+        if(this.questions[this.current_index].answer) this.toNextStep()
       },
       toNextStep() {
         this.current_index++
+        this.was_validated = false
       },
+      toPreventStep() {
+        this.current_index--
+        this.was_validated = false
+      }
+    },
+    mounted() {
+      this.questions = this.questionsProp
     }
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,24 @@
   <div class="home bg-dark text-white">
     <div class="container d-flex flex-column align-items-center justify-content-center min-vh-100 pb-5">
       <h1 class="text-center mb-5 mt-3">Vue-quiz</h1>
-      <div class="mb-lg-5">
-          <Quiz />
+      <div class="col-lg-5 mb-lg-5">
+          <Quiz v-if="QUESTIONS.length" :questions-prop="QUESTIONS"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { QUESTIONS } from '@/api/questions'
 import Quiz from '@/components/Quiz.vue'
 
 export default {
   name: 'Home',
   components: {
     Quiz
+  },
+  created() {
+    this.QUESTIONS = QUESTIONS
   }
 }
 </script>
