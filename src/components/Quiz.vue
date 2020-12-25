e3431f2a9db93c4db1c52124c66adcc9f945c50f
AndrewBeznosko <andrew.beznosko@gmail.com>
2020-12-25 14:12:11 +0200

    validation continue

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="vue-quiz">
-    <div class="card bg-secondary">
+    <form 
+      class="card bg-secondary"
+      :class="{ 'was-validated' : was_validated }"
+    >
       <div class="card-header position-relative border-0">
         <h2 class="fw-bold mb-1">Question question question?</h2>
         <div class="progress position-absolute w-100 start-0 bottom-0 rounded-0" style="height: 2px;">
@@ -9,29 +12,39 @@
       </div>
       <div class="card-body py-5">
         <div class="card">
-          <!-- is-invalid -->
-          <!-- was-validated -->  
-          <form class="was-validated list-group list-group-flush text-dark">
+          <div class="list-group list-group-flush text-dark">
+
             <div class="form-check list-group-item ps-5">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" required>
-              <label class="form-check-label fw-bold" for="flexRadioDefault1">
+              <input 
+                class="form-check-input" 
+                type="radio" 
+                name="flexRadioDefault" 
+                id="flexRadioDefault1" 
+                required
+              >
+              <label 
+                class="form-check-label fw-bold" 
+                for="flexRadioDefault1"
+              >
                 Default radio
               </label>
             </div>
+
             <div class="form-check list-group-item  ps-5">
               <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" required>
               <label class="form-check-label fw-bold" for="flexRadioDefault2">
                 Default checked radio
               </label>
             </div>
-          </form>
+
+          </div>
         </div>
       </div>
       <div class="card-footer d-flex justify-content-between">
-        <a href="#" class="btn btn-outline-light">Preview</a>
-        <a href="#" class="btn btn-primary ml-auto fw-bold">Step</a>
+        <button type="button" class="btn btn-outline-light disabled">Preview</button>
+        <button @click.prevent="validate($event)" type="button" class="btn btn-primary ml-auto fw-bold">Step</button>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -40,6 +53,22 @@
     name: 'Quiz',
     props: {
       msg: String
+    },
+    data() {
+      return {
+        was_validated: false,
+        current_index: 0
+      }
+    },
+    methods: {
+      validate(e) {
+        e.target.closest('form').requestSubmit()
+        this.was_validated = true
+        this.toNextStep()
+      },
+      toNextStep() {
+        this.current_index++
+      },
     }
   }
 </script>
